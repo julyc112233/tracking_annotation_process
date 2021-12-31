75eb9a5029a86c0ae3bdc92f314b431e27bf6286
zeng weijia <weijia.zeng@aifi.com>
2021-12-31 15:31:36 +0800

    filter.ipynb for filter out the pair of people-id which can be merge to same people
    web_step1.py for annotator to working on the step1 of annotation
    
    web_step2.py for annotator to working on the step2 of annotation

--- web_step1.py
+++ web_step1.py
@@ -1,5 +1,5 @@
 from pywebio.input import actions, input_group
-from pywebio.output import clear, put_html, put_row
+from pywebio.output import clear, put_html, put_row, put_text
 from pywebio.platform.tornado import start_server
 import pandas as pd
 import re
@@ -7,16 +7,21 @@ from collections import defaultdict
 import typing
 from dataclasses import dataclass
 import uuid
+import logging
+import time
+import os
 
 
-token="sp=r&st=2021-12-30T07:26:27Z&se=2022-02-01T15:26:27Z&sv=2020-08-04&sr=c&sig=D78d1irOmMA3sEKRL%2FGaH88%2FwppCQlP0DNu2dndYzM0%3D"
+token = "sp=r&st=2021-12-30T07:26:27Z&se=2022-02-01T15:26:27Z&sv=2020-08-04&sr=c&sig=D78d1irOmMA3sEKRL%2FGaH88%2FwppCQlP0DNu2dndYzM0%3D"
+
+
 @dataclass
 class Row:
     id: str
     video: str
-    duration:float
+    duration: float
     thumbnail: str
-    camera_id:int
+    camera_id: int
 
     @classmethod
     def from_str(cls, str):
@@ -51,10 +56,13 @@ def get_raw_string(encode: str):
             res.append(encode[starts[i][0] : starts[i + 1][0] - 2])
     return res
 
+
 def trans_url(url):
-    infos=url.split('?')
-    res=infos[0]+"?"+token
+    infos = url.split("?")
+    res = infos[0] + "?" + token
     return res
+
+
 def get_urls_from_str(content):
     Rowstrings = get_raw_string(content)
     res = []
@@ -64,7 +72,7 @@ def get_urls_from_str(content):
     return res
 
 
-def display(index1, index2, df):
+def display(index1, index2, df, text="Unchecked!"):
     urls_first = get_urls_from_str(df.loc[index1, "videos"])
     urls_second = get_urls_from_str(df.loc[index2, "videos"])
     put_row(
@@ -91,13 +99,14 @@ def display(index1, index2, df):
         ],
         size="50% 10px 50%",
     )
-
+    put_text(text)
     info = input_group(
         "Check video",
         [
             actions(
                 "",
                 [
+                    {"label": "Next", "value": 2},
                     {"label": "Yes", "value": 1},
                     {"label": "No", "value": 0, "color": "danger"},
                     {"label": "save", "value": -1},
@@ -106,6 +115,7 @@ def display(index1, index2, df):
             ),
         ],
     )
+
     return info
 
 
@@ -118,6 +128,7 @@ def get_unannotate_hat_case(path=".csv"):
 
 
 def update_file(df, path="step1.csv"):
+    print("write to ", path)
     df.to_csv(path)
 
 
@@ -131,12 +142,31 @@ def get_unchecked_id(id_to_index, df):
     return targ_id
 
 
+def get_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    rq = time.strftime("%Y%m%d%H%M", time.localtime(time.time()))
+    log_path = os.path.dirname(os.getcwd()) + "/Logs/"
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    log_name = os.path.join(log_path + rq + "log_step1.log")
+    logfile = log_name
+    fh = logging.FileHandler(logfile, mode="w")
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s"
+    )
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
+
+
 def main(path):
+    logger = get_logger()
     df = pd.read_csv(path)
     if not "checked" in df.columns.values:
         df["checked"] = "NO"
 
-    process_list = []
     id_to_index = defaultdict(list)
     for index, row in df.iterrows():
         id = row["corrected_shopper_id"]
@@ -154,12 +184,37 @@ def main(path):
                 continue
             if df.loc[index, "checked"] == "YES":
                 continue
-            info = display(index, index_tmp, df)
-            if info["check"] == 0:
-                df.loc[index, "corrected_shopper_id"] = uuid.uuid1()
-            if info["check"] == -1:
-                update_file(df)
-            clear()
+            Flag = None
+            while True:
+                if Flag == None:
+                    text = "Unchecked!"
+                elif Flag == False:
+                    text = "Not Same person!"
+                else:
+                    text = "Same person!"
+                info = display(index, index_tmp, df, text)
+                logger.info(
+                    "index1:"
+                    + str(index)
+                    + " index2:"
+                    + str(index_tmp)
+                    + " info:"
+                    + str(info)
+                )
+                # print(index,index_tmp,info)
+                clear()
+                if info["check"] == 0:
+                    Flag = False
+                if info["check"] == -1:
+                    if Flag == False:
+                        df.loc[index, "corrected_shopper_id"] = uuid.uuid1()
+                    update_file(df)
+                if info["check"] == 1:
+                    Flag = True
+                if info["check"] == 2:
+                    if Flag == False:
+                        df.loc[index, "corrected_shopper_id"] = uuid.uuid1()
+                    break
         update_file(df)
 
 
--- web_step2.py
+++ web_step2.py
@@ -1,5 +1,5 @@
 from pywebio.input import actions, input_group
-from pywebio.output import clear, put_html, put_row
+from pywebio.output import clear, put_html, put_row, put_text
 from pywebio.platform.tornado import start_server
 import pandas as pd
 import re
@@ -7,15 +7,20 @@ from collections import defaultdict
 import typing
 from dataclasses import dataclass
 import uuid
+import logging
+import time
+import os
 
-token="sp=r&st=2021-12-30T07:26:27Z&se=2022-02-01T15:26:27Z&sv=2020-08-04&sr=c&sig=D78d1irOmMA3sEKRL%2FGaH88%2FwppCQlP0DNu2dndYzM0%3D"
+token = "sp=r&st=2021-12-30T07:26:27Z&se=2022-02-01T15:26:27Z&sv=2020-08-04&sr=c&sig=D78d1irOmMA3sEKRL%2FGaH88%2FwppCQlP0DNu2dndYzM0%3D"
+
+
 @dataclass
 class Row:
     id: str
     video: str
-    duration:float
+    duration: float
     thumbnail: str
-    camera_id:int
+    camera_id: int
 
     @classmethod
     def from_str(cls, str):
@@ -50,10 +55,13 @@ def get_raw_string(encode: str):
             res.append(encode[starts[i][0] : starts[i + 1][0] - 2])
     return res
 
+
 def trans_url(url):
-    infos=url.split('?')
-    res=infos[0]+"?"+token
+    infos = url.split("?")
+    res = infos[0] + "?" + token
     return res
+
+
 def get_urls_from_str(content):
     Rowstrings = get_raw_string(content)
     res = []
@@ -63,7 +71,7 @@ def get_urls_from_str(content):
     return res
 
 
-def display(index1, index2, df):
+def display(index1, index2, df, text="Unchecked!"):
     urls_first = get_urls_from_str(df.loc[index1, "videos"])
     urls_second = get_urls_from_str(df.loc[index2, "videos"])
     put_row(
@@ -90,13 +98,14 @@ def display(index1, index2, df):
         ],
         size="50% 10px 50%",
     )
-
+    put_text(text)
     info = input_group(
         "Check video",
         [
             actions(
                 "",
                 [
+                    {"label": "Next", "value": 2},
                     {"label": "Yes", "value": 1},
                     {"label": "No", "value": 0, "color": "danger"},
                     {"label": "save", "value": -1},
@@ -105,14 +114,18 @@ def display(index1, index2, df):
             ),
         ],
     )
+
     return info
 
 
-
-def update_file(df,book, path="step2.csv"):
-    for index,row in df.iterrows():
-        df.loc[index,"corrected_shopper_id"]=book[row["corrected_shopper_id"]]
+def update_file(df, book, path="step2.csv"):
+    for index, row in df.iterrows():
+        df.loc[index, "corrected_shopper_id"] = find(
+            str(row["corrected_shopper_id"]), book
+        )
+    print("write to ", path)
     df.to_csv(path)
+
 
 def get_unchecked_id(id_to_index, df):
     targ_id = None
@@ -126,52 +139,111 @@ def get_unchecked_id(id_to_index, df):
     #     df.loc[index,"checked"]="PENDING"
     return targ_id
 
+
 def get_valid_pair(infos):
-    res=[]
+    res = []
     for info in infos:
-        strs=info.split(",")
-        id1=strs[0][1:]
-        id2=strs[1][1:-1]
-        res.append((id1,id2))
+        strs = info.split(",")
+        id1 = strs[0][1:]
+        id2 = strs[1][1:-1]
+        res.append((id1, id2))
     return res
 
-def find(id,book):
-    if book[id]==id:
+
+def find(id, book):
+    if book[id] == id:
         return id
     else:
-        x=find(book[id],book)
-        book[id]=x
+        x = find(book[id], book)
+        book[id] = x
         return x
-def unite(id1,id2,book):
-    id1=find(id1,book)
-    id2=find(id2,book)
-    book[id1]=id2
+
+
+def unite(id1, id2, book):
+    id1 = find(id1, book)
+    id2 = find(id2, book)
+    # print(id1,id2)
+    book[id1] = id2
+    # print(find(id1,book),find(id2,book))
     return
 
-def main(data_path,pair_path):
+
+def get_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    rq = time.strftime("%Y%m%d%H%M", time.localtime(time.time()))
+    log_path = os.path.dirname(os.getcwd()) + rq + "/Logs/"
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    log_name = os.path.join(log_path + "log_step2.log")
+    logfile = log_name
+    fh = logging.FileHandler(logfile, mode="w")
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s"
+    )
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
+
+
+def main(data_path, pair_path):
+    logger = get_logger()
     df = pd.read_csv(data_path)
-    df_pair=pd.read_csv(pair_path)
-    valid_pair=get_valid_pair(df_pair["0"].tolist())
+    df_pair = pd.read_csv(pair_path)
+    valid_pair = get_valid_pair(df_pair["0"].tolist())
     id_to_index = defaultdict(list)
-    book={}
+    book = {}
     for index, row in df.iterrows():
         id = str(row["corrected_shopper_id"])
         id_to_index[id].append(index)
-        book[id]=id
-    for x,y in valid_pair:
-        index1=id_to_index[x][0]
-        index2=id_to_index[y][0]
-        info=display(index1,index2,df)
-        if info["check"]==1:
-            unite(x,y)
-        if info["check"]==-1:
-            update_file(df,book)
-        clear()
-    update_file(df,book)
+        book[id] = id
+    for x, y in valid_pair:
+        index1 = id_to_index[x][0]
+        index2 = id_to_index[y][0]
+        Flag = None
+        while True:
+            if Flag == None:
+                text = "Unchecked!"
+            elif Flag == False:
+                text = "Not Same person!"
+            else:
+                text = "Same person!"
+            info = display(index1, index2, df, text)
+            logger.info(
+                "index1:"
+                + str(index1)
+                + " index2:"
+                + str(index2)
+                + " info:"
+                + str(info)
+            )
+            print(
+                "index1:"
+                + str(index1)
+                + " index2:"
+                + str(index2)
+                + " info:"
+                + str(info)
+            )
+            clear()
+            if info["check"] == 0:
+                Flag = False
+            if info["check"] == -1:
+                if Flag == True:
+                    unite(x, y, book)
+                update_file(df, book)
+            if info["check"] == 1:
+                Flag = True
+            if info["check"] == 2:
+                if Flag == True:
+                    unite(x, y, book)
+                break
+    update_file(df, book, book)
 
 
 if __name__ == "__main__":
     path = "~/Downloads/merged_new_hat_table.csv"
-    pair_path="~/Downloads/se.csv"
-    main(path,pair_path)
+    pair_path = "~/Downloads/se.csv"
+    main(path, pair_path)
     # start_server(main)
